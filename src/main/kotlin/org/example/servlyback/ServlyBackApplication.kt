9a73f7c1a6e733cd92e76ecf3415b95664976509
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-15 14:12:26 +0100

    Initial commit

---
+++ src/main/kotlin/org/example/servlyback/ServlyBackApplication.kt
@@ -0,0 +1,11 @@
+package org.example.servlyback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServlyBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServlyBackApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/servlyback/ServlyBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.servlyback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServlyBackApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
