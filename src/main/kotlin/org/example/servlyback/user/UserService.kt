8c69fab04833f27c0cd9eab4c2d5989132158dd1
Nerekk <arkadiusz.salaj.02@gmail.com>
2025-01-21 23:08:22 +0100

    Role services implementation

--- src/main/kotlin/org/example/servlyback/user/UserService.kt
+++ src/main/kotlin/org/example/servlyback/user/UserService.kt
@@ -1,17 +1,66 @@
 package org.example.servlyback.user
 
+import com.google.firebase.auth.FirebaseAuth
+import org.example.servlyback.entities.User
+import org.example.servlyback.entities.custom_fields.Role
+import org.example.servlyback.security.firebase.TokenManager
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(private val userRepository: UserRepository) {
 
-    fun getAllUsers(): List<User> = userRepository.findAll()
+    fun getUserRoles(): ResponseEntity<Role> {
+        val firebaseToken = TokenManager.getFirebaseToken() ?: return ResponseEntity(HttpStatus.UNAUTHORIZED)
+        val uid = firebaseToken.uid
+        val user = userRepository.findByUid(uid)
 
-    fun getUserById(id: Long): User? = userRepository.findById(id).orElse(null)
+        if (user != null) {
+            return ResponseEntity(user.role, HttpStatus.OK)
+        } else {
+            val newUser = User(
+                uid = uid,
+                email = getEmailByUid(uid)
+            )
+            userRepository.save(newUser)
 
-    fun getUserByEmail(email: String): User? = userRepository.findByEmail(email)
+            return ResponseEntity(newUser.role, HttpStatus.OK)
+        }
+    }
 
-    fun saveUser(user: User): User = userRepository.save(user)
+    fun addUserRole(role: Role) {
+        val firebaseToken = TokenManager.getFirebaseToken() ?: return
+        val uid = firebaseToken.uid
+        val user = userRepository.findByUid(uid)
 
-    fun deleteUser(id: Long) = userRepository.deleteById(id)
+        if (user != null) {
+            when (role) {
+                Role.CUSTOMER -> {
+                    when (user.role) {
+                        Role.NONE -> user.role = role
+                        Role.PROVIDER -> user.role = Role.BOTH
+                        else -> {}
+                    }
+                }
+                Role.PROVIDER -> {
+                    when (user.role) {
+                        Role.NONE -> user.role = role
+                        Role.CUSTOMER -> user.role = Role.BOTH
+                        else -> {}
+                    }
+                }
+                else -> {}
+            }
+
+            userRepository.save(user)
+        }
+    }
+
+    fun getEmailByUid(uid: String): String {
+        val userRecord = FirebaseAuth.getInstance().getUser(uid)
+        return userRecord.email
+    }
+
+
 }
